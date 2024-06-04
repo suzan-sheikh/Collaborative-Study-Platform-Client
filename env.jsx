19316e0f0d340dcac2e6d69ce47f1aaa08426428
suzan-sheikh <mdsuzanskh@gmail.com>
2024-06-04 07:42:07 +0600

    for office use

---
+++ env.jsx
@@ -0,0 +1,11 @@
+VITE_apiKey=AIzaSyAg5W6Xey_gK_R9VpvdbARdwGboGVoiilM,
+VITE_authDomain=brainbond-e920d.firebaseapp.com,
+VITE_projectId=brainbond-e920d,
+VITE_storageBucket=brainbond-e920d.appspot.com,
+VITE_messagingSenderId=835553369983,
+VITE_appId=1:835553369983:web:4903f8327e3fc48b18044a,
+
+# VITE_IMGBB_API_KEY=c14a543e4fab6a67e7a3bb9c04924f62
+
+VITE_API_URL='http://localhost:8000'
+# VITE_API_URL='https://jobhunter-ten.vercel.app'
