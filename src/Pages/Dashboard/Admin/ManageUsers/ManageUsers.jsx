a52432c6d6c0af876c764e808cf842499d3cb0a6
suzan-sheikh <mdsuzanskh@gmail.com>
2024-06-09 23:47:16 +0600

    user role update complited

--- src/Pages/Dashboard/Admin/ManageUsers/ManageUsers.jsx
+++ src/Pages/Dashboard/Admin/ManageUsers/ManageUsers.jsx
@@ -1,69 +1,87 @@
-
-import { Helmet } from "react-helmet"
-import MangeUserRow from "./MangeUserRow"
+import { Helmet } from "react-helmet";
+import MangeUserRow from "./MangeUserRow";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import Loader from "../../../Loader/Loader";
 
 const ManageUsers = () => {
+  const axiosSecure = useAxiosSecure();
+
+  const {
+    data: users = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const { data } = await axiosSecure(`/users`);
+      return data;
+    },
+  });
+
+  if (isLoading) return <Loader />;
+
   return (
     <>
       <Helmet>
         <title>Admin | Manage Users</title>
       </Helmet>
 
-      <div className='container mx-auto px-4 sm:px-8'>
-        <div className='py-8'>
-          <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
+      <div className="container mx-auto px-4 sm:px-8">
+        <div className="py-8">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="flex p-1 overflow-hidden bg-[#003430] justify-center border-b border-gray-200">
+              <input
+                className="px-4 py-1 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
+                placeholder="Search hear"
+              />
 
+              <button className="px-1 md:px-4 py-1 text-sm font-medium tracking-wider bg-emerald-500 hover:bg-blue-500 text-white transition-all">
+                Search
+              </button>
+            </div>
 
-          <div className="flex p-1 overflow-hidden bg-[#003430] justify-center border-b border-gray-200">
-            <input
-              className="px-4 py-1 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
-              placeholder="Search hear"
-            />
-
-            <button className="px-1 md:px-4 py-1 text-sm font-medium tracking-wider bg-emerald-500 hover:bg-blue-500 text-white transition-all">
-              Search
-            </button>
-          </div>
-
-
-            <div className='inline-block min-w-full shadow rounded-sm overflow-hidden'>
-              <table className='min-w-full leading-normal'>
+            <div className="inline-block min-w-full shadow rounded-sm overflow-hidden">
+              <table className="min-w-full leading-normal">
                 <thead>
-                  <tr>                     
+                  <tr>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-[#003430]  border-b border-[#003430] text-white  text-center text-sm uppercase font-medium'
+                      scope="col"
+                      className="px-5 py-3 bg-[#003430]  border-b border-[#003430] text-white  text-center text-sm uppercase font-medium"
                     >
                       Serial
-                    </th>                      
+                    </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-[#003430]  border-b border-[#003430] text-white  text-center text-sm uppercase font-medium'
+                      scope="col"
+                      className="px-5 py-3 bg-[#003430]  border-b border-[#003430] text-white  text-center text-sm uppercase font-medium"
                     >
                       Name
-                    </th>                   
+                    </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-[#003430]  border-b border-[#003430] text-white  text-center text-sm uppercase font-medium'
+                      scope="col"
+                      className="px-5 py-3 bg-[#003430]  border-b border-[#003430] text-white  text-center text-sm uppercase font-medium"
                     >
                       Email
-                    </th>                     
+                    </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-[#003430]  border-b border-[#003430] text-white  text-center text-sm uppercase font-medium'
+                      scope="col"
+                      className="px-5 py-3 bg-[#003430]  border-b border-[#003430] text-white  text-center text-sm uppercase font-medium"
                     >
                       Role
-                    </th>                     
+                    </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-[#003430]  border-b border-[#003430] text-white  text-center text-sm uppercase font-medium'
+                      scope="col"
+                      className="px-5 py-3 bg-[#003430]  border-b border-[#003430] text-white  text-center text-sm uppercase font-medium"
                     >
                       Change Role
-                    </th>                      
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
-                  <MangeUserRow/>
+                  {users.map((user, index) => (
+                    <MangeUserRow key={user._id} user={user} refetch={refetch} index={index} />                   
+                  ))}
+                  
                 </tbody>
               </table>
             </div>
@@ -71,7 +89,7 @@ const ManageUsers = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ManageUsers
+export default ManageUsers;
--- src/Pages/Dashboard/Admin/ManageUsers/MangeUserRow.jsx
+++ src/Pages/Dashboard/Admin/ManageUsers/MangeUserRow.jsx
@@ -1,43 +1,90 @@
 import PropTypes from "prop-types";
 import { AiOutlineEdit } from "react-icons/ai";
-import { MdDelete } from "react-icons/md";
+import useAuth from "../../../../hooks/useAuth";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { useState } from "react";
+import UpdateUserModal from "./UpdateUserModal";
 
-const MangeUserRow = () => {
+const MangeUserRow = ({user, refetch, index}) => {
+
+  const {user: loggedInUser} = useAuth()
+  const axiosSecure = useAxiosSecure()
+  const [isOpen, setIsOpen] = useState(false);
+
+  const {mutateAsync} = useMutation({
+    mutationFn: async role => {
+      const {data} = await axiosSecure.patch(`/users/update/${user?.email}`, role)
+      return data
+    },
+    onSuccess: data => {
+      console.log(data);
+      toast.success('role updated')
+      refetch()
+      setIsOpen(false)
+    }
+  })
+
+
+  const modalHandler = (selected) => {
+      console.log(loggedInUser, user?.email);
+    if(loggedInUser?.email === user?.email) {
+
+      toast.error('Action Not Allowed')
+      return setIsOpen(false)
+    }
+    const role = {
+      role: selected,
+    }
+    mutateAsync(role)
+  };
+
+
+
   return (
     <tr>
       <td className="px-2 py-2 border-b border-gray-200 bg-white text-sm">
         <p className="text-gray-900 whitespace-no-wrap text-center">
-          01
+         {index+1}
         </p>
       </td>
       <td className="px-2 py-2  border-b border-gray-200 bg-white text-sm">
         <p className="text-gray-900 whitespace-no-wrap text-center">
-         Md Suzan Sheikh
+          {user?.name}
         </p>
       </td>
       <td className="px-2 py-2  border-b border-gray-200 bg-white text-sm">
         <p className="text-gray-900 whitespace-no-wrap text-center">
-          mdsuzan@gmail.com
+        {user?.email}
         </p>
       </td>
       <td className="px-2 py-2  border-b border-gray-200 bg-white text-sm">
         <p className="text-gray-900 whitespace-no-wrap text-center">
-          Student
+        {user?.role}
         </p>
       </td>
       <td className="px-2 py-2  border-b border-gray-200 bg-white text-sm mx-auto">
         <div className="flex justify-center">
-          <button className="bg-[#003430] hover:bg-[#42CE9F] px-3 py-1 rounded-sm transition-all">
+          <button onClick={() => setIsOpen(true)} className="text-white flex items-center gap-2 bg-[#003430] hover:bg-[#42CE9F] px-3 py-1 rounded-sm transition-all">
             <AiOutlineEdit className="text-white text-lg font-black" />
+            Update Role
           </button>
         </div>
+        <UpdateUserModal
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          modalHandler={modalHandler}
+          user={user}
+        />
       </td>
     </tr>
   );
 };
 
 MangeUserRow.propTypes = {
-  booking: PropTypes.object,
+  user: PropTypes.object,
+  index: PropTypes.number,
   refetch: PropTypes.func,
 };
 
